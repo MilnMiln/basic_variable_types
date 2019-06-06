c1ac583da134c12c6eb790b7098e9e11ceac8414
MilnMiln <miln@gmx.com>
2019-06-06 15:30:32 -0700

    Updated exercise.py Added more examples and techniques.

--- exercise.py
+++ exercise.py
@@ -1,9 +1,23 @@
 #! /usr/bin/python3
 
+# Floats
 myFloat = 7.1
+
+# Integers
 myInt = 2
+a, b = 3, 4
+
+# Strings
 myString = "This is a string."
 
-print ("Floats: ", myFloat,float(myInt)) 
-print ("Integers: ", myInt, int(myFloat))
+if isinstance(myFloat, float):
+    print ("myFloat is a float-type.")
+
+if isinstance(myInt, int):
+    print ("myInt is an integer.")
+
+print ("Floats: %f, " % myFloat, float(myInt)) 
+
+print ("Integers: ", myInt, int(myFloat), a, b)
+
 print ("Strings: ", myString)
