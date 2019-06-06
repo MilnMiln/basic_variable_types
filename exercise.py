0256e3fa8efedee690a21c8e38afff0d181c4c2f
MilnMiln <miln@gmx.com>
2019-06-06 14:42:50 -0700

    Added README.md and exercise.py.

---
+++ exercise.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+myFloat = 7.1
+myInt = 2
+myString = "This is a string."
+
+print ("Floats: ", myFloat,float(myInt)) 
+print ("Integers: ", myInt, int(myFloat))
+print ("Strings: ", myString)
